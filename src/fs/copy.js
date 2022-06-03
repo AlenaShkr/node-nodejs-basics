5e379c3ae826bbae38484d7d4d57118e33935143
”AlenaShkr” <elalshkr@gmail.com>
2022-06-03 22:42:34 +0300

    nodejs-basics/feat: add copy method for fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,4 +1,14 @@
+import fs from 'node:fs/promises';
+
 export const copy = async () => {
-    
+    let copyURL = 'src/fs/files';
+    try{
+        await fs.mkdir(`${copyURL}_copy`, {recursive: false});
+        await fs.readdir(copyURL)
+        .then(resolve => resolve.forEach(el => fs.copyFile(`${copyURL}/${el}`, `${copyURL}_copy/${el}`)));
+
+    } catch(err) {
+        throw new Error('FS operation failed');
+    }
 };
 copy();
