ad95c0b69adf6fca3f07974cd10b004dfa1ab340
”AlenaShkr” <elalshkr@gmail.com>
2022-06-03 23:29:25 +0300

    nodejs-basics/feat: add rename method for fs

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,12 @@
+import fs from 'node:fs/promises';
+
 export const rename = async () => {
-    // Write your code here 
+    const renamedFile = 'src/fs/files_copy/wrongFilename.txt';
+    const renamingFile = 'src/fs/files_copy/properFilename.md';
+    try {
+        await fs.rename(renamedFile, renamingFile);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    } 
 };
+rename();
