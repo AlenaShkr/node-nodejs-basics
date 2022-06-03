5a64911a5b19ba5a98e97433a87bdc609e58db92
”AlenaShkr” <elalshkr@gmail.com>
2022-06-03 20:27:27 +0300

    nodejs-basics/fix:change error message  method create for fs

--- src/fs/create.js
+++ src/fs/create.js
@@ -12,7 +12,7 @@ export const create = async () => {
             })
             return;
         } 
-        throw new Error('file just exist');
+        throw new Error('FS operation failed');
     })
     
 };
