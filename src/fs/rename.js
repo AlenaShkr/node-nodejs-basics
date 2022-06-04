a1f31cf3dacb6f62909c1593b8e1be84da67e1a6
”AlenaShkr” <elalshkr@gmail.com>
2022-06-04 17:51:01 +0300

    nodejs-basics/fix: fix rename method for fs

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -4,13 +4,13 @@ export const rename = async () => {
     const renamedFile = 'src/fs/files/wrongFilename.txt';
     const renamingFile = 'src/fs/files/properFilename.md';
     fs.stat(renamingFile, (err, stat) => {
-        if (err) {
-            fs.rename(renamedFile, renamingFile, (err) =>{
-                if(err) {
-                    throw new Error('FS operation failed');
-                }
-            }); 
-        } else throw new Error('FS operation failed');
-    })
-};
+        if(typeof stat === 'object') {
+            throw new Error('FS operation failed');
+        } else {
+            fs.rename(renamedFile, renamingFile, (err, res) => {
+                if(err) throw new Error('FS operation failed');
+            })
+        } 
+    });          
+}
 rename();
