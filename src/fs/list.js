c6ec7488cc70963d433faeaf5e901392a260ae89
”AlenaShkr” <elalshkr@gmail.com>
2022-06-04 19:16:13 +0300

    nodejs-basics/feat: add list method for fs

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,15 @@
+import fs from 'node:fs/promises';
+
 export const list = async () => {
-    // Write your code here 
+    let dirURL = 'src/fs/files';
+    try{
+         await fs.readdir(dirURL)
+        .then( res => {
+            console.log(`in directory:`);
+            res.forEach(el => console.log(el));
+        })
+    } catch(error) {
+        throw new Error('FS operation failed');
+    }
 };
+list();
