92663da958d616d3f11c932daeb6dabdc6e1d126
”AlenaShkr” <elalshkr@gmail.com>
2022-06-04 20:41:56 +0300

    nodejs-basics/fix: fix env for cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -2,6 +2,6 @@ export const parseEnv = () => {
     const envVariable = process.env;
     let parsedEnvVariable = (Object.entries(envVariable)
     .map(([key, value]) => [`RS_${key}=`, `${value}`])).join(';  ');
-    console.log(parsedEnvVariable);
+    process.stdout.write(parsedEnvVariable);
 };
 parseEnv();
