922e4637be0da5bdc3022b595a5efd73303c766a
”AlenaShkr” <elalshkr@gmail.com>
2022-06-04 19:02:39 +0300

    nodejs-basics/feat: add delete  method for fs

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,11 @@
+import fs from 'node:fs/promises';
+
 export const remove = async () => {
-    // Write your code here 
+    let deleteURL = 'src/fs/files/fileToRemove.txt';
+    try {
+        await fs.rm(deleteURL, {force: false, recursive: true});
+    } catch(error) {
+        throw new Error('FS operation failed')
+    }
 };
+remove();
