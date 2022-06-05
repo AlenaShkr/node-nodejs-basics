d99323d23ee631bd4812e76e6ba8aedb91224b74
”AlenaShkr” <elalshkr@gmail.com>
2022-06-05 23:38:25 +0300

    nodejs-basics/fix: fix  spawn

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,7 +1,7 @@
 import childProcess from 'node:child_process';
 
 export const spawnChildProcess = async (args) => {
-    const child = childProcess.fork('src/cp/files/script.js');
+    const child = childProcess.fork('src/cp/files/script.js', args);
     child.on('error', (error) => {
         throw new Error('some problem');
     })
