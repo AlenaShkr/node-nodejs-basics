bf4abeae917cdb71255a666d7bae978d58535c39
”AlenaShkr” <elalshkr@gmail.com>
2022-06-05 15:14:27 +0300

    nodejs-basics/feat: add transform  method for stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,19 @@
+import { Transform, pipeline } from 'node:stream';
+
 export const transform = async () => {
-    // Write your code here 
+    const readStream = process.stdin;
+    const writeStream = process.stdout;
+
+    const upperCaseTransform = new Transform({
+        transform(data, encoding, callback) {
+            this.push(data.toString().toUpperCase());
+            callback();
+        }
+    });
+     pipeline(readStream, upperCaseTransform,  writeStream, err => {
+         if (error) {
+             throw new Error('Some problem')
+         }
+     });
 };
+transform();
