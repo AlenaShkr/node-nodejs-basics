3a728b22925ceb007b4e03af2044abd1c566a4e9
”AlenaShkr” <elalshkr@gmail.com>
2022-06-05 23:35:52 +0300

    nodejs-basics/fix: add on error in  spawnn

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -2,5 +2,8 @@ import childProcess from 'node:child_process';
 
 export const spawnChildProcess = async (args) => {
     const child = childProcess.fork('src/cp/files/script.js');
+    child.on('error', (error) => {
+        throw new Error('some problem');
+    })
 };
 spawnChildProcess();
